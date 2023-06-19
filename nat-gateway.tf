26843022b6acaf48086fb447e61ad901a025f14a
Samuel Haddison <samuelhaddison71@gmail.com>
2023-06-19 14:07:35 +0100

    modified eip resource blcck

--- nat-gateway.tf
+++ nat-gateway.tf
@@ -1,6 +1,6 @@
 # allocate elastic ip. this eip will be used for the nat-gateway in public subnet az1 
 resource "aws_eip" "eip_for_nat_gateway_az1" {
-  vpc = true
+  domain = "vpc"
 
   tags = {
     Name = "eip-az1"
